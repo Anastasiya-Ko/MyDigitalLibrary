0c5ee39b5221eccd9d6a5feb719deff9c8450850
Anastasiya <agropina_90@mail.ru>
2023-06-20 13:55:24 +0300

    add: добавила аннотацию для проверки половой принадлежности перечислению.

---
+++ src/main/java/ru/kopylova/springcourse/DigitalLibrary/util/valid/genger/GenderNamePattern.java
@@ -0,0 +1,26 @@
+package ru.kopylova.springcourse.DigitalLibrary.util.valid.genger;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD})
+@Retention(RUNTIME)
+@Constraint(validatedBy = GenderNamePatternValidator.class)
+@Documented
+public @interface GenderNamePattern {
+    Class<? extends Enum<?>> enumClass();
+
+    String message() default "Поле должно соответствовать: Муж - для мужчины, Жен - для женщины";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
